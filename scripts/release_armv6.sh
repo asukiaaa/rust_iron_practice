f4ae7b255fc79231876b872c3f18f2f7d0517055
Asuk Kono <asukiaaa@gmail.com>
2019-02-23 21:14:03 +0900

    Add script to release for arvm6

---
+++ scripts/release_armv6.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+PROJECT_NAME=rust_iron_practice
+BUILD_TARGET=arm-unknown-linux-gnueabi
+RELEASE_DIR=armv6
+
+cargo build --target ${BUILD_TARGET} --release
+
+if [ -d "releases/${RELEASE_DIR}" ]; then
+    rm -r releases/${RELEASE_DIR}
+fi
+mkdir -p releases/${RELEASE_DIR}
+cp target/${BUILD_TARGET}/release/${PROJECT_NAME} releases/${RELEASE_DIR}/
+cp -r public releases/${RELEASE_DIR}/
+cp -r templates releases/${RELEASE_DIR}/
+cd releases
+if [ -f "${RELEASE_DIR}.zip" ]; then
+    rm -r ${RELEASE_DIR}.zip
+fi
+zip -r ${RELEASE_DIR}.zip ${RELEASE_DIR}
+cd ../
